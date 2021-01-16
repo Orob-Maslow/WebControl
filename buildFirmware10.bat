ae54d4b531d5ded8049010fe59e755994fb98d8e
Orob-Maslow <59507087+Orob-Maslow@users.noreply.github.com>
2021-01-16 00:03:35 -0600

    Syntax change is -> ==
    the comparison using "is" checks identity while == is an equivalence value check.  When checking for a file name, it is an equivalence check.

--- buildFirmware10.bat
+++ buildFirmware10.bat
@@ -13,10 +13,10 @@ mkdir holey
 
 cd ../build
 SET beta_firmware_repo=https://github.com/WebControlCNC/Firmware.git
-SET beta_firmware_sha=bf4350ffd9bc154832505fc0125abd2c4c04dba7
+SET beta_firmware="beta-test_features"
 git clone %beta_firmware_repo% firmware/beta
 cd firmware/beta
-git checkout %beta_firmware_sha%
+git checkout %beta_firmware%
 pio run -e megaatmega2560
 
 copy .pio/build/megaatmega2560/firmware.hex ~/WebControl/firmware/beta/beta-$(sed -n -e 's/^.*VERSIONNUMBER //p' cnc_ctrl_v1/Maslow.h).hex
@@ -25,18 +25,17 @@ cd ../..
 
 SET webcontrolcnc_firmware_repo=https://github.com/WebControlCNC/Firmware.git
 SET webcontrolcnc_firmware_sha=e1e0d020fff1f4f7c6b403a26a85a16546b7e15b
-git clone %webcontrolcnc_firmware_repo% firmware/maslowcnc
+git clone %webcontrolcnc_firmware_repo% firmware/webcontrolcnc
 cd firmware/webcontrolcnc
-git checkout %webcontrolcnc_firmware_sha%
 pio run -e megaatmega2560
 copy .pio/build/megaatmega2560/firmware.hex ~/WebControl/firmware/webcontrolcnc/webcontrolcnc-$(sed -n -e 's/^.*VERSIONNUMBER //p' cnc_ctrl_v1/Maslow.h).hex
 
 cd ../..
 
-SET holey_firmware_repo=https://github.com/webcontrolcnc/Firmware.git
-SET holey_firmware_sha=950fb23396171cbd456c2d4149455cc45f5e6bc3
+SET holey_firmware_repo=https://github.com/WebControlCNC/Firmware.git
+SET holey_branch="holey-firmware"
 git clone %holey_firmware_repo% firmware/holey
 cd firmware/holey
-git checkout %holey_firmware_sha%
+git checkout %holey_branch%
 pio run -e megaatmega2560
 copy .pio/build/megaatmega2560/firmware.hex ~\WebControlfirmware/holey/holey-$(sed -n -e 's/^.*VERSIONNUMBER //p' cnc_ctrl_v1/Maslow.h).hex
